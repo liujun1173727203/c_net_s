17dc345295aebd2a2196c3976d23273439830cfb
liujun1173727203 <1173727203@qq.com>
2023-12-05 05:45:32 -0800

    xxx

---
+++ demo2_p/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"swap.h"
+using namespace std;
+
+int main(){
+	int a=3,b=4;
+	double f=0.0;
+	swap(a,b);
+	cout<<a<<","<<b<<endl;	
+	cout<<"hello world"<<endl;
+	return 0;
+}
